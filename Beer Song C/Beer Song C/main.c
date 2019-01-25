342964360b6ae8a431ac18e2244fe6baf0693e5c
nancySoprano <kasiandraa@gmail.com>
2019-01-25 12:15:03 +0100

    99 bottles of beer song in C

---
+++ Beer Song C/Beer Song C/main.c
@@ -0,0 +1,32 @@
+//
+//  main.c
+//  Beer Song C
+//
+//  Created by Nancy Soprano on 25.01.2019.
+//  Copyright © 2019 Nancy Soprano. All rights reserved.
+//
+
+#include <stdio.h>
+
+void song(int bottles) {
+    
+    if (bottles == 0){
+        printf("There are simply no more bottles of beer on the wall.\n\n");
+    } else if (bottles == 1){
+        printf("%d bottle of beer on the wall, %d bottle of beer.\n\nTake one down, pass it around. No more bottles of beer on the wall.\n\nGo to the store and buy some more, 99 bottles of beer on the wall.\n\n", bottles, bottles);
+      
+        
+    } else {
+        printf("%d bottles of beer on the wall, %d bottles of beer.\n", bottles, bottles);
+        int oneLess = bottles - 1;
+        printf("Take one down, pass it around, %d bottles of beer on the wall.\n\n", oneLess);
+        song(oneLess);
+        
+    }
+    }
+
+
+int main(int argc, const char * argv[]) {
+    song(4);
+    return 0;
+}
