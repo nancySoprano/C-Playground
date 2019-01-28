b77c7def92b90720a0b7aac121d5914d45645dc0
nancySoprano <kasiandraa@gmail.com>
2019-01-28 23:00:14 +0100

    for loop

--- Coolness/Coolness/main.c
+++ Coolness/Coolness/main.c
@@ -10,10 +10,16 @@
 
 int main(int argc, const char * argv[]) {
     //while loop
-    int i = 0; //an integer that will kick off the looping
-    while (i < 12) { //loop as long as i is less than 12
-        printf("%d. Elon is Cool\n", i); //%d only to make the counting-looping more visible
-        i++; //increase the value of "i" after each loop, so that the loop can eventually end.
+//    int i = 0; //an integer that will kick off the looping. INITIALIZATION
+//    while (i < 12) { //loop as long as i is less than 12
+//        printf("%d. Elon is Cool\n", i); //%d only to make the counting-looping more visible
+//        i++; //increase the value of "i" after each loop, so that the loop can eventually end.
+//    }
+//    return 0;
+    
+//    the same thing as a for loop
+    for (int i = 0; i < 12; i++){ //(some initialization, some check, some last step)
+        printf("%d. Elon is Cool\n", i);
     }
     return 0;
 }
