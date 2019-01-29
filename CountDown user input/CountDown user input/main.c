a003ee114073b5cd804018848c6b07177ddc13e0
nancySoprano <kasiandraa@gmail.com>
2019-01-29 21:33:25 +0100

    Chapter 8 Challenge: user input complete

--- CountDown user input/CountDown user input/main.c
+++ CountDown user input/CountDown user input/main.c
@@ -1,14 +1,14 @@
 //
 //  main.c
-//  Challenge: counting down
+//  CountDown user input
 //
 //  Created by Nancy Soprano on 29/01/2019.
 //  Copyright © 2019 Nancy Soprano. All rights reserved.
 //
 
 #include <stdio.h>
-#import <stdio.h>
-#include <readline/readline.h>
+#include <readline/readline.h> //readline
+#include <stdlib.h> //atoi
 
 int main(int argc, const char * argv[]) {
     //Challenge: counting down
@@ -17,7 +17,11 @@ int main(int argc, const char * argv[]) {
     
     //Challenge: user input
     // modify the program to ask for input from the user
-    for (int i = 99; i >= 0; i -= 3) {
+    printf("What number should start the countdown?");
+    const char *numberInt = readline(NULL);
+    int number = atoi(numberInt);
+    
+    for (int i = number; i >= 0; i -= 3) {
         if (i % 5 == 0) {
             printf("%d\nFound one!\n", i);
         } else {
