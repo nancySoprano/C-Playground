7610dff4650d819a7fdfbc7ae2870d926fa78e48
nancySoprano <kasiandraa@gmail.com>
2019-02-06 11:46:11 +0100

    c training

--- Addresses/Addresses/main.c
+++ Addresses/Addresses/main.c
@@ -11,8 +11,12 @@
 //The address of a variable is the location in memory where the value for that variable is stored. To get the variable address, you use the "&" operator.
 int main(int argc, const char * argv[]) {
     int i = 17;
-    int *addressOfI = &i;
+    int *addressOfI = &i; // ! it will hold an address where an int can be stored
     printf("i stores its value at %p\n", addressOfI);
-    printf("this function starts at %p\n", main); //number in memory at which the number is stored
+// printf("this function starts at %p\n", main); //number in memory at which the number is stored
+//printf("the int stored at addressOfI is %d\n", *addressOfI); // * -> will display the value of the integer stored at addresOfI. // asterix used to read the value that is stored at the address stored in addressOfI
+    *addressOfI = 89; //use the * operator on the left hand side of an assignement to store data at a particular addess.
+    printf("Now i is %d\n", i);
+    
     return 0;
 }
